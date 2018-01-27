3884ebf58312bee23297360b14a3d9ce6deb4c08
Henrik van Linn <henrik.van-linn@tum.de>
2018-01-27 11:29:54 +0100

    Added Types.

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+export declare function generateVAPIDKeys(): VAPIDKeys;
+
+export declare function setGCMAPIKey(key: string): void;
+
+export declare function encrypt(userPublicKey: string, userAuth: string, payload: string): EncryptedData;
+
+export declare function getVapidHeaders(audience: string, subject: string, publicKey: string, privateKey: string): EncryptedData;
+
+export declare function generateRequestDetails(pushSubscription: PushSubscription, payload?: string | Buffer, options?: PushOptions)
+
+export declare function setVapidDetails(mail: string, publicKey: string, privateKey: string): void;
+
+export declare function sendNotification(pushSubscription: PushSubscription, payload?: string | Buffer, options?: PushOptions): Promise<PushResponse>;
+
+export interface PushSubscription {
+    endpoint: string;
+    keys: {
+        auth: string;
+        p256dh: string;
+    }
+}
+
+export interface PushOptions {
+    gcmAPIKey: string;
+    vapidDetails: VAPIDKeys & {
+        subject: string;
+    };
+    TTL: number;
+    headers: {
+        [key: string]: string;
+    }
+    agent: any
+}
+
+interface VAPIDKeys {
+    publicKey: string;
+    privateKey: string;
+}
+
+interface EncryptedData {
+    localPublicKey: string;
+    salt: string;
+    cipherText: Buffer;
+}
+
+interface RequestDetails {
+    endpoint: string;
+    method: string;
+    headers: {
+        [key: string]: string;
+    };
+    body: Buffer;
+}
+
+interface PushResponse {
+    statusCode: number;
+    headers: {
+        [key: string]: string;
+    };
+    body: any;
+}
